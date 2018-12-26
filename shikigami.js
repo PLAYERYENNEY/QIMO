4ce360f51c627b68578db4fa84653893e6081cd0
PLAYERYENNEY <45528225+PLAYERYENNEY@users.noreply.github.com>
2018-12-26 15:06:27 +0800

    Create shikigami.js

---
+++ shikigami.js
@@ -0,0 +1,37 @@
+{
+  "shikigami" : "SHIKIGAMI TYPE",
+  "type" : "输出",
+  "typeone" :" 辅助",
+  "typetwo" : "控制"
+  "secretBase" : "Super tower",
+  "active" : true,
+  "members" : [
+    {
+      "Type" : "输出",
+      "describe" : "对敌方造成伤害",
+      "eg" : [
+        "酒吞童子 / SHUTEN DOJI",
+        "大天狗 / OOTENGU",
+        "荒 / SUSABI"
+      ]
+    },
+    {
+      "Type" : "辅助",
+      "describ" : "对友方进行帮助",
+      "powers" : [
+        "一目连 ICHIMOKUREN",
+        "辉夜姬 / KAGUYA",
+        "花鸟卷 / HANA"
+      ]
+    },
+    {
+      "Type" : "控制",
+      "describe" : "对敌方造成控制效果",
+      "powers" : [
+        "阎魔 / ENMA",
+        "兵俑 / SAMURAI X",
+        "般若 / HANNYA"
+      ]
+    }
+  ]
+}
